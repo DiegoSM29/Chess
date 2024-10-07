d85aa9272825862c00577f1d4267b05839d2d92a
unknown <diegoselayamolina@gmail.com>
2024-10-07 18:36:24 -0400

    Queen functionality added

--- src/app/pieces/queen.js
+++ src/app/pieces/queen.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 
 export default class Queen {
-  constructor(numberOfPLayer){
+  constructor(numberOfPLayer) {
     this.name = "Reyna";
     this.numberOfPLayer = numberOfPLayer;
     // this.initialPos = initialPos;
-    this.image = <Image src={numberOfPLayer === 1 ? "/images/ReinaBlanca.png" : "/images/ReinaNegra.png"} alt="torre" width={65} height={65}/>   
+    this.image = <Image src={numberOfPLayer === 1 ? "/images/ReinaBlanca.png" : "/images/ReinaNegra.png"} alt="torre" width={65} height={65} />
   }
   canMove(y, x, playerTurn, board) {
     let minorY = Math.min(this.pos.y, y) + 1;
@@ -15,23 +15,23 @@ export default class Queen {
     if (largestX - minorX === largestY - minorY && playerTurn === this.numberOfPLayer) {
       let otherList = board.slice(minorY, largestY);
       otherList = otherList.map((row, indexY) => {
-        let rows = row.slice(minorX, largestX);
-        if (          
+        let subList = row.slice(minorX, largestX);
+        if (
           (this.pos.x > x && this.pos.y < y) ||
           (this.pos.x < x && this.pos.y > y)
         ) {
-          return rows.filter((p, indexX) => indexY + indexX === row.length - 1);
+          return subList.filter((p, indexX) => indexY + indexX === subList.length - 1);
         }
-        return rows.filter((p, indexX) => indexY === indexX);
+        return subList.filter((p, indexX) => indexY === indexX);
       });
       return !otherList.flat(Infinity).some(e => e);
-    }else if (playerTurn === this.numberOfPLayer && this.pos.y === y && this.pos.x !== x){
+    } else if (playerTurn === this.numberOfPLayer && this.pos.y === y && this.pos.x !== x) {
       return !board[y].slice(minorX, largestX).some(e => e);
-    }else if (playerTurn === this.numberOfPLayer && this.pos.x === x && this.pos.y !== y){
+    } else if (playerTurn === this.numberOfPLayer && this.pos.x === x && this.pos.y !== y) {
       let isThereSomePiece = board.map(e => {
-        e = e.filter((piece,posPiece) => posPiece === x ? e : null);
+        e = e.filter((piece, posPiece) => posPiece === x ? e : null);
         return e;
-      });      
+      });
       return !isThereSomePiece.flat(Infinity).slice(minorY, largestY).some(e => e);
     }
     return false;
